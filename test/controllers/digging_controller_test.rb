9179b9d0b30ee49a2bdfc13da74d1c2c9ac42b41
Fabian Müller <fmueller83@googlemail.com>
2022-02-22 14:50:50 +0100

    Adds integration test for user authentication

--- test/controllers/digging_controller_test.rb
+++ test/controllers/digging_controller_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class DiggingControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+  end
+
+  test "confirmed user can see home page after login" do
+    get user_session_path
+    assert_equal 200, status
+    post user_session_path, params: {'user[email]' => @user.email, 'user[password]' =>  "helloworld"}
+    follow_redirect!
+
+    assert_equal 200, status
+    assert_equal "/", path
+  end
+
+  test "increase amount of sand after dig" do
+    sign_in(@user)
+    assert_changes -> { @user.sand } do
+      post dig_url
+    end
+  end
 end
