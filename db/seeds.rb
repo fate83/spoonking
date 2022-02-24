0eb255c358bfcb90605863b96fd36aeda292f90f
Fabian Müller <fmueller83@googlemail.com>
2022-02-24 16:55:58 +0100

    Adds some items to backpack in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
-Item.delete_all
+BackpackItem.destroy_all
+
+Item.destroy_all
 %w(Apfelkitsch Altmetall).each do |name|
   Item.create!(name: name)
 end
 
-User.delete_all
-User.create!(
+User.destroy_all
+user1 = User.create!(
   email:                 "fate@primus-fatum.de",
   password:              "install123",
   password_confirmation: "install123",
   confirmed_at:          DateTime.now
 )
+
+user1.backpack = Item.all
