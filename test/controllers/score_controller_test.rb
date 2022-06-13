43b12534f63b1ca96009f1ff0a8fca69c451d7e4
Fabian Müller <fmueller83@googlemail.com>
2022-06-13 13:49:52 +0200

    Fix test for GET highscore list

--- test/controllers/score_controller_test.rb
+++ test/controllers/score_controller_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class ScoreControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get index" do
-    get score_index_url
+    @user = users(:one)
+    sign_in(@user)
+    get top10_url
     assert_response :success
   end
 end
